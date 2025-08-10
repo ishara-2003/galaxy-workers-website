c6185e49e52867c0403aa440a1d784c23dee38da
ishara-2003 <isharasandaruwanudagama@gmail.com>
2025-08-10 08:56:42 -0400

    Update integrate-callback.js

--- apii/integrate-callback.js
+++ apii/integrate-callback.js
@@ -4,7 +4,7 @@ export default async function handler(req, res) {
             const { code } = req.query;
            
             const response = await fetch(`https://graph.facebook.com/v17.0/oauth/access_token?client_id=1282075310376377
-&client_secret=4a3ce4f658d88e408d42443dbd5caa4c&code=${code}&redirect_uri=${encodeURIComponent('YOUR_REDIRECT_URI')}`);
+&client_secret=4a3ce4f658d88e408d42443dbd5caa4c&code=${code}&redirect_uri=${encodeURIComponent('https://galaxy-workers.vercel.app/api/integrate-callback')}`);
             const data = await response.json();
           
             res.redirect('/?integrated=true');
