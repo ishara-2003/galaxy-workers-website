f11d17c47e40583f3543400330f446ed79ef09ff
ishara-2003 <isharasandaruwanudagama@gmail.com>
2025-08-10 08:55:56 -0400

    Update integrate.js

--- apii/integrate.js
+++ apii/integrate.js
@@ -3,7 +3,7 @@ export default async function handler(req, res) {
         try {
           
             const { code } = req.body;
-            const response = await fetch(`https://graph.facebook.com/v17.0/oauth/access_token?client_id=1282075310376377&client_secret=4a3ce4f658d88e408d42443dbd5caa4c&code=${code}&redirect_uri=${encodeURIComponent('YOUR_REDIRECT_URI')}`);
+            const response = await fetch(`https://graph.facebook.com/v17.0/oauth/access_token?client_id=1282075310376377&client_secret=4a3ce4f658d88e408d42443dbd5caa4c&code=${code}&redirect_uri=${encodeURIComponent('https://galaxy-workers.vercel.app/api/integrate.js')}`);
             const data = await response.json();
             res.status(200).json({ success: true, accessToken: data.access_token });
         } catch (error) {
